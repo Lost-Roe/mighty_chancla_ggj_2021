f4a65d24ab4d24bca90af2913104889d38578ad6
Hyde Wyvern <hyde.s.wyvern@gmail.com>
2021-01-30 06:28:11 -0600

    Energy Consumption and Recharge system implemented.

---
+++ Assets/Enemy AI/scripts/player/EnergyConsumer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyConsumer : MonoBehaviour
+{
+    [SerializeField]
+    private EnergySystem energySystem;
+    [SerializeField]
+    private PauseSystem pause;
+    [SerializeField]
+    private Light right_eye;
+    [SerializeField]
+    private Light left_eye;
+
+    private float maxLightIntensity;
+
+    public float consumptionRate;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxLightIntensity = right_eye.intensity;
+        energySystem.ResetEnergySystem();
+        InvokeRepeating("ConsumeEnergy", consumptionRate, consumptionRate);
+    }
+
+    public void ConsumeEnergy()
+    {
+        var e = energySystem.currentEnergy;
+
+        if(pause.value == false)
+        {
+            energySystem.currentEnergy = Mathf.Clamp(e + (1 * energySystem.consuptionMultiplier),0,energySystem.maxEnergy);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        right_eye.intensity = Mathf.Clamp(maxLightIntensity * (energySystem.currentEnergy / 100), 0, maxLightIntensity);
+        left_eye.intensity = right_eye.intensity;
+    }
+}
