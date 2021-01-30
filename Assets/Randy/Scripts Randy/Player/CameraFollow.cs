50560ef52b5baf15b460fc009f4f8d1641f6b95b
Randres <andplayer230@gmail.com>
2021-01-29 23:16:08 -0600

    Seguimiento de camara con el jugador

--- Assets/Randy/Scripts Randy/Player/CameraFollow.cs
+++ Assets/Randy/Scripts Randy/Player/CameraFollow.cs
@@ -9,15 +9,21 @@ public class CameraFollow : MonoBehaviour
     [Range(0.1f, 1f)]
     [SerializeField] private float smoothFactor = 0;
 
+    [SerializeField] private bool lookAtPlayer = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraOffest = transform.position - playerTransform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 newPos = playerTransform.position + cameraOffest;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        if (lookAtPlayer)
+            transform.LookAt(playerTransform);
     }
 }
